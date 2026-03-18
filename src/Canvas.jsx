06cdea4c7d79d59f8b7db21f3a7ce3b7a0b99be4
JosuaWelsche <josua.welsche@stud.kbw.ch>
2026-03-18 11:53:47 +0100

    refactor: tshape to original shape

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -16,11 +16,10 @@ const Canvas = ({width, height, gap}) => {
 
 
     const tshape = [
-        {y: -1, x: -1},
         {y: -1, x: 0},
-        {y: -1, x: 1},
+        {y: 0, x: -1},
         {y: 0, x: 0},
-        {y: 1, x: 0}
+        {y: 0, x: +1}
     ]
 
     const oshape = [
