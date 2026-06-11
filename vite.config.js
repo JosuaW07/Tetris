7dfa446ba31a384c1e970cdd10305f9208684bac
JosuaWelsche <josua.welsche@stud.kbw.ch>
2026-06-11 11:15:46 +0200

    fix: vite config for gh-pages and open shift

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
-    base: "/Tetris/",
+    base: isGitHubPages ? '/Tetris/' : '/',
     plugins: [react()],
 })
